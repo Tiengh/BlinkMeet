17107bfe72d999756f3ae7ba875c39cdc92325c5
Tien Nguyen <tienguyen231@gmail.com>
2026-09-14 23:44:33 +0700

    fix: use env url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.MODE === "development" ? "http://localhost:5001/api" : "/api";
+  import.meta.env.VITE_API_URL || "http://localhost:5001/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
